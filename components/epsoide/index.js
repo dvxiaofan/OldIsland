36680a1407c3c40fb2de4664e54ea2e7ee62729e
xiaofan <1353815633@qq.com>
2018-09-28 15:38:29 +0800

    使用wxs处理数据

--- components/epsoide/index.js
+++ components/epsoide/index.js
@@ -6,13 +6,13 @@ Component({
   properties: {
     index: {
       type: String,
-      observer: function (newVal, oldVal, changedPath) {
-        let val = newVal < 10 ? '0' + newVal : newVal;
-        // 不能在observer里修改自身属性， 因为observer 本身就是监测数据变化，setData后会再次调用observer，会出现无限递归现象
-        this.setData({
-          _index: val
-        })        
-      }
+      // observer: function (newVal, oldVal, changedPath) {
+      //   let val = newVal < 10 ? '0' + newVal : newVal;
+      //   // 不能在observer里修改自身属性， 因为observer 本身就是监测数据变化，setData后会再次调用observer，会出现无限递归现象
+      //   this.setData({
+      //     _index: val
+      //   })        
+      // }
     }
   },
 
@@ -22,7 +22,7 @@ Component({
   data: {
     year: 0,
     month: '',
-    _index: ''
+    // _index: ''
   },
 
   /**
