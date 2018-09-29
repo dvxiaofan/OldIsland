1b45317b7f84c4ccfce1d29ab85f7ff811082b39
xiaofan <1353815633@qq.com>
2018-09-29 17:19:49 +0800

    数据实现缓存处理

--- models/classic.js
+++ models/classic.js
@@ -7,17 +7,27 @@ class ClassicModel extends HTTP {
       success: res => {
         sCallBack(res);
         this._setLatestIndex(res.index);
+        let key = this._getKey(res.index);
+        wx.setStorageSync(key, res);
       }
     })
   }
   
   getClassicData(index, nextOrPrev, sCallBack) {
-    this.request({
-      url: 'classic/' + index + '/' + nextOrPrev,
-      success: res => {
-        sCallBack(res);
-      }
-    })
+    // 查看缓存中是否存在数据，如有，就不加载
+    let key = nextOrPrev == 'next' ? this._getKey(index + 1) : this._getKey(index - 1);
+    let classic = wx.getStorageSync(key);
+    if (!classic) {
+      this.request({
+        url: 'classic/' + index + '/' + nextOrPrev,
+        success: res => {
+          wx.setStorageSync(this._getKey(res.index), res)
+          sCallBack(res);
+        }
+      })
+    } else {
+      sCallBack(classic);
+    }
   }
 
   isFirst(index) {
@@ -38,6 +48,12 @@ class ClassicModel extends HTTP {
   _getLatestIndex() {
     let index = wx.getStorageSync('latest');
     return index;
+  }
+
+  // 设定一个缓存key
+  _getKey(index) {
+    let key = 'classic-' + index;
+    return key;
   }
 }
 
