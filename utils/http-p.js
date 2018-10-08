376b0662fc2e0a28581aa18aea8a2b6719b7893c
xiaofan <1353815633@qq.com>
2018-10-08 18:43:37 +0800

    利用promise方法获取书单列表信息

--- utils/http-p.js
+++ utils/http-p.js
@@ -1,4 +1,6 @@
-import { config } from '../config.js';
+import {
+	config
+} from '../config.js';
 
 const tips = {
 	1: '抱歉，出现一个小错误',
@@ -6,28 +8,33 @@ const tips = {
 }
 
 class HTTP {
-	request(params) {
-		if (!params.method) {
-			params.method = 'GET'
-		}
+	request({url, data={}, method='GET'}) {
+		return new Promise((resolve, reject) => {
+			this._request(url, resolve, reject, data, method);
+		});
+	}
+
+	_request(url, resolve, reject, data={}, method='GET') {
 		wx.request({
-			url: config.api_base_url + params.url,
-			data: params.data,
-			method: params.method,
+			url: config.api_base_url + url,
+			data: data,
+			method: method,
 			header: {
 				'content-type': 'application/json',
-				'appkey': config.appkey
+				'appkey': config.appkey,
 			}, 
-			success: (res) =>{
-				let code = res.statusCode.toString();				
+			success: res =>{
+				const code = res.statusCode.toString();				
 				if (code.startsWith('2')) {
-					params.success && params.success(res.data);
+					resolve(res.data);
 				} else {
-					let error_code = res.data.error_code;
+					reject();
+					const error_code = res.data.error_code;
 					this._show_error(error_code);
 				}
 			},
 			fail: () => {
+				reject();
 				this._show_error(1);
 			}
 		})
