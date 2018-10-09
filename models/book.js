62ec934de9e8c745fada3531073a99b470f8339f
xiaofan <1353815633@qq.com>
2018-10-09 17:22:24 +0800

    详情页短评组件设置

--- models/book.js
+++ models/book.js
@@ -7,13 +7,31 @@ class BookModel extends HTTP {
 		return this.request({
 			url: 'book/hot_list'
 		});
-	}
+	};
 
 	getMyBookCount() {
 		return this.request({
 			url: 'book/favor/count'
 		});
-	}
+	};
+
+	getDetail(bid) {
+		return this.request({
+			url: `book/${bid}/detail`
+		});
+	};
+
+	getLikeStatus(bid) {
+		return this.request({
+			url: `book/${bid}/favor`
+		});
+	};
+
+	getComments(bid) {
+		return this.request({
+			url: `book/${bid}/short_comment`
+		});
+	};
 }
 
 
