f89ba2d19049f9cfa6eed3d41acae2ea4edda0d5
xiaofan <1353815633@qq.com>
2018-10-16 11:47:35 +0800

    搜索组件显示与隐藏

--- pages/book/book.js
+++ pages/book/book.js
@@ -9,7 +9,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-    books: []
+    books: [],
+    searching: false,
   },
 
   /**
@@ -22,6 +23,18 @@ Page({
           books: res
         })
       })
+  },
+
+  onSearching(e) {
+    this.setData({
+      searching: true
+    })
+  },
+
+  onCancel(e) {
+    this.setData({
+      searching: false
+    })
   },
 
   /**
